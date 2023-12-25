4f74133e198957779a13224431ff2920ddcdd37a
Miguel Tomas Silva <mtpsilva@gmail.com>
2023-12-25 20:14:57 +0100

    update

--- products/concrete/js/agency.js
+++ products/concrete/js/agency.js
@@ -25,7 +25,6 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-
 $('div.modal').on('show.bs.modal', function() {
 	var modal = this;
 	var hash = modal.id;
